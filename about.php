a7dd14160fe8a74c3534abc2f7214b311112f6f6
trung32kj <trung01694011@gmail.com>
2025-05-07 18:35:19 +0700

    Refactor styling and structure for the about page; update icon IDs and improve layout consistency

--- about.php
+++ about.php
@@ -60,29 +60,39 @@ $brand_result = mysqli_query($conn, $brand_sql);
 
         width: 50%;
     }
-    i{
+    #i{
         width: 30px;
         height: 30px;
-        border-radius: 50%;
+        border-radius: 40%;
+        padding: 5px;
         float: left;
         color: blue;
     }
     .row3 {
         width: 100%;
-        margin-top: 20px;
+        margin: 40px 0;
         text-align: center;
-        
+
     }
     .row3-top {
         padding: 20px;
+        margin-top: 50px;
+
     }
     .row3-bottom1 {
-        width: 30%;
+        width: 390px;
+        height: 200px;
         padding: 20px;
         border: 1px solid black;
         display: inline-block;
         margin: 0 20px;
         float: left;
+    }
+    .row4 {
+        width: 100%;
+        margin: 40px 0;
+        text-align: center;
+
     }
 
 </style>
@@ -115,41 +125,55 @@ $brand_result = mysqli_query($conn, $brand_sql);
                 </div>
                 <div class="bottom">
                     <div class="bottom-left">
-                        <i class="fa-regular fa-circle-check"></i>
+                        <i id="i" class="fa-regular fa-circle-check"></i>
                         <h4>Sản phẩm chính hãng</h4>
                         <p>100% sản phẩm từ các thương hiệu uy tín và được kiểm chứng chất lượng.</p>
                     </div>
                     <div class="bottom-right">
-                        <i class="fa-solid fa-car-side"></i>
+                        <i id="i" class="fa-solid fa-car-side"></i>
                         <H4>Giao hàng nhanh chóng</H4>
                         <P>Đảm bảo giao hàng nhanh chóng và an toàn trong vòng 24h.</P>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row3" >
+        <div class="row4" >
             <div class="row3-top">
                 <h4>Những Giá Trị Cốt Lõi</h4>
                 <p>Chúng tôi luôn hướng đến những giá trị cốt lõi để mang đến trải nghiệm tốt nhất cho khách hàng</p>
             </div>
             <div class="row3-bottom">
                 <div class="row3-bottom1">
-                    
-                    <h4><i class="fa-solid fa-star"></i>Chất lượng hàng đầu</h4>
+                    <h4><i id="i" class="fa-solid fa-star"></i>Chất lượng hàng đầu</h4>
                     <p>Cam kết cung cấp sản phẩm sữa với chất lượng cao nhất, đảm bảo dinh dưỡng và an toàn cho sức khỏe người tiêu dùng.</p>
                 </div>
                 <div class="row3-bottom1">
-                    <h4><i class="fa-solid fa-users"></i>Khách Hàng</h4>
+                    <h4><i id="i" class="fa-solid fa-users"></i>Khách Hàng</h4>
                     <p>Đặt khách hàng vào trung tâm của mọi quyết định, mang đến trải nghiệm mua sắm thuận tiện và dịch vụ chăm sóc tận tâm.</p>
                 </div>
                 <div class="row3-bottom1">
-                    <H4><i class="fa-solid fa-leaf"></i>Bền Vững</H4>
+                    <H4><i id="i" class="fa-solid fa-leaf"></i>Bền Vững</H4>
                     <P>Cam kết phát triển bền vững, sử dụng bao bì thân thiện với môi trường và hỗ trợ các nhà sản xuất có trách nhiệm.</P>
                 </div>
             </div>
-        </div>
-        <div class="row4" >
-            *****
+            <div class="row3-top">
+                <h4>Điều Gì Khiến Milky Trở Nên Đặc Biệt?</h4>
+                
+            </div>
+            <div class="row3-bottom">
+                <div class="row3-bottom1">
+                    <h4><i id="i" class="fa-solid fa-star"></i>Sản Phẩm Chính Hãng 100%</h4>
+                    <p>Chúng tôi cam kết chỉ cung cấp sản phẩm chính hãng từ các thương hiệu lớn, uy tín trên thế giới.</p>
+                </div>
+                <div class="row3-bottom1">
+                    <h4><i id="i" class="fa-solid fa-users"></i>Giá Cả Cạnh Tranh</h4>
+                    <p>Với chính sách giá hợp lý cùng nhiều chương trình khuyến mãi hấp dẫn, chúng tôi mang đến lựa chọn tiết kiệm nhất cho khách hàng.</p>
+                </div>
+                <div class="row3-bottom1">
+                    <H4><i id="i" class="fa-solid fa-leaf"></i>Dịch Vụ Khách Hàng 24/7</H4>
+                    <P>Đội ngũ tư vấn và chăm sóc khách hàng sẵn sàng hỗ trợ mọi lúc, mọi nơi để đảm bảo trải nghiệm mua sắm trọn vẹn.</P>
+                </div>
+            </div>
         </div>
         <div class="row5" >
             *****

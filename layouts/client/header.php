f335b47adf440e74b2c16ba820e9ee45a8c069df
phmhuy1710 <huy_2351220234@dau.edu.vn>
2025-05-07 17:54:54 +0700

    Add new product and brand tables, update queries, and enhance product display

--- layouts/client/header.php
+++ layouts/client/header.php
@@ -15,7 +15,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="/assets/images/logo.png">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <!-- Google Fonts -->

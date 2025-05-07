bd0074570c62e8610b3e165a054434e9f8d5cc97
trung32kj <trung01694011@gmail.com>
2025-05-07 17:50:48 +0700

    Enhance about page layout and styling; replace images with icons and add core values section

--- about.php
+++ about.php
@@ -60,10 +60,28 @@ $brand_result = mysqli_query($conn, $brand_sql);
 
         width: 50%;
     }
-    #img1{
-        width: 40px;
-        height: 40px;
+    i{
+        width: 30px;
+        height: 30px;
         border-radius: 50%;
+        float: left;
+        color: blue;
+    }
+    .row3 {
+        width: 100%;
+        margin-top: 20px;
+        text-align: center;
+        
+    }
+    .row3-top {
+        padding: 20px;
+    }
+    .row3-bottom1 {
+        width: 30%;
+        padding: 20px;
+        border: 1px solid black;
+        display: inline-block;
+        margin: 0 20px;
         float: left;
     }
 
@@ -97,12 +115,12 @@ $brand_result = mysqli_query($conn, $brand_sql);
                 </div>
                 <div class="bottom">
                     <div class="bottom-left">
-                        <img id="img1" src="https://img.pikbest.com/png-images/qiantu/yellow-tick-pattern_2626970.png!sw800" alt="">
+                        <i class="fa-regular fa-circle-check"></i>
                         <h4>Sản phẩm chính hãng</h4>
                         <p>100% sản phẩm từ các thương hiệu uy tín và được kiểm chứng chất lượng.</p>
                     </div>
                     <div class="bottom-right">
-                        <img id="img1" src="https://media.istockphoto.com/id/1068362158/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-giao-h%C3%A0ng.jpg?s=612x612&w=0&k=20&c=anJxVTe25N3oE5lOAa8ktngpgrwhi5KtFDsVJuSx8EA=" alt="">
+                        <i class="fa-solid fa-car-side"></i>
                         <H4>Giao hàng nhanh chóng</H4>
                         <P>Đảm bảo giao hàng nhanh chóng và an toàn trong vòng 24h.</P>
                     </div>
@@ -110,7 +128,25 @@ $brand_result = mysqli_query($conn, $brand_sql);
             </div>
         </div>
         <div class="row3" >
-            
+            <div class="row3-top">
+                <h4>Những Giá Trị Cốt Lõi</h4>
+                <p>Chúng tôi luôn hướng đến những giá trị cốt lõi để mang đến trải nghiệm tốt nhất cho khách hàng</p>
+            </div>
+            <div class="row3-bottom">
+                <div class="row3-bottom1">
+                    
+                    <h4><i class="fa-solid fa-star"></i>Chất lượng hàng đầu</h4>
+                    <p>Cam kết cung cấp sản phẩm sữa với chất lượng cao nhất, đảm bảo dinh dưỡng và an toàn cho sức khỏe người tiêu dùng.</p>
+                </div>
+                <div class="row3-bottom1">
+                    <h4><i class="fa-solid fa-users"></i>Khách Hàng</h4>
+                    <p>Đặt khách hàng vào trung tâm của mọi quyết định, mang đến trải nghiệm mua sắm thuận tiện và dịch vụ chăm sóc tận tâm.</p>
+                </div>
+                <div class="row3-bottom1">
+                    <H4><i class="fa-solid fa-leaf"></i>Bền Vững</H4>
+                    <P>Cam kết phát triển bền vững, sử dụng bao bì thân thiện với môi trường và hỗ trợ các nhà sản xuất có trách nhiệm.</P>
+                </div>
+            </div>
         </div>
         <div class="row4" >
             *****
